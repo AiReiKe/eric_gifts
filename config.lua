022b477f2c615f3801cd9cfea0eca5366670f596
AiReiKe <78699906+AiReiKe@users.noreply.github.com>
2021-02-08 22:25:02 +0800

    Update config.lua

--- config.lua
+++ config.lua
@@ -6,7 +6,7 @@ Config.giftname = '新手禮包'  --禮包名稱
 Config.command = 'gift' --自訂指令名稱
 Config.giftpack = true  --false 是直接獲取禮包內容物
 
-Config.Item = { --禮包內容物, bank為銀行錢, cash為身上錢
+Config.Item = { --禮包內容物, bank為銀行錢, cash為背包錢
     {name = 'cash', count = 200000},
     {name = 'bank', count = 100000},
     {name = 'bread', count = 15},
