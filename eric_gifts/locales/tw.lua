61447d7b592b68dfc569d01ceef2e9d72c0f3dc1
AiReiKe <78699906+AiReiKe@users.noreply.github.com>
2021-06-18 09:44:35 +0800

    Delete tw.lua

--- eric_gifts/locales/tw.lua
+++
@@ -1,8 +0,0 @@
-Locales['tw'] = {
-    ['haved_gift'] = '領過了還想再領一次啊',
-    ['getting_gift'] = '正在獲取 ~g~%s~w~...',
-    ['got_gift'] = '獲得了 ~g~%s',
-    ['received_car'] = '你已收到一台車牌為 ~y~%s ~s~的新手載具, 快去車庫看看吧!',
-    ['unknown_car'] = '請通知管理員新手車 %s 為~r~未知的車輛',
-    ['open_gift'] = '你開啟了 ~g~%s~w~, 祝你遊玩愉快'
-}
