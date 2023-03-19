58baac71a83ad0de302103cdf42017760443866c
AiReiKe <78699906+AiReiKe@users.noreply.github.com>
2023-03-19 18:57:34 +0800

    Add files via upload

--- locales/tw.lua
+++ locales/tw.lua
@@ -1,9 +1,11 @@
 Locales['tw'] = {
     ['haved_gift'] = '領過了還想再領一次啊',
-    ['getting_gift'] = '正在獲取 ~g~%s~w~...',
+    ['getting_gift'] = '正在獲取 ~g~%s~s~...',
     ['got_gift'] = '獲得了 ~g~%s',
     ["get_gift"] = '獲取 %s',
     ['received_car'] = '你已收到一台車牌為 ~y~%s ~s~的新手載具, 快去車庫看看吧!',
     ['unknown_car'] = '請通知管理員新手車 %s 為~r~未知的車輛',
-    ['open_gift'] = '你開啟了 ~g~%s~w~, 祝你遊玩愉快'
+    ['open_gift'] = '你開啟了 ~g~%s~s~, 祝你遊玩愉快',
+    ['press_to_get'] = '按 ~g~[E]~s~ 領取新手禮包',
+    ['blip_name'] = '新手禮包'
 }
